d1e9c481bb66623c597cdf406ef838b433293ea3
Ike Mattice <ike.mattice@alaskaair.com>
2023-05-18 10:00:41 -0700

    Initial Commit

---
+++ ColorSchemeManager/ColorSchemeManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSchemeManagerApp.swift
+//  ColorSchemeManager
+//
+//  Created by Ike Mattice on 5/18/23.
+//
+
+import SwiftUI
+
+@main
+struct ColorSchemeManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
